b0acfb2e7e9e7d631052da6aa8bdcc2d4ab0c452
Todd Miller <toddses@gmail.com>
2015-09-01 18:48:45 -0400

    update menu

--- content/themes/os-flux/index.php
+++ content/themes/os-flux/index.php
@@ -61,12 +61,10 @@ get_header(); ?>
                         <p class="bold">Saturdays</p>
                         <p>Every Saturday between 9am and 1pm you can find us at the <a href="https://www.facebook.com/huberheightsfarmersmarket" target="_blank" class="text-pink500">Huber Heights Farmer's Market</a></p>
                         <p><a href="https://goo.gl/maps/2kSkT" target="_blank" class="text-pink500">6178 Chambersburg Road<br />Huber Heights, OH 45424</a></p>
-                        <h4>This Week's Menu, August 15</h4>
+                        <h4>This Week's Menu, September 5</h4>
                         <p>Sandwich Bread by the Loaf<br />
                         Cinnamon Rolls<br />
-                        Apple and Blueberry Cobbler<br />
-                        Strawberry and Raspberry Danish<br />
-                        Apple Cinnamon Sugar Cookies</p>
+                        Blueberry Pie!</p>
                         <p>We'll be accepting pre-orders until Wednesday evening! Send 'em over with the form below!</p>
                     </div>
                 </div>
